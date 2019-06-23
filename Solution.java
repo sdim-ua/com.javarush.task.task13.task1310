f801fcbca964d4431a6a4a4782479b5af008b548
sdim-ua <ingersol@yandex.ua>
2019-06-23 21:54:25 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,46 @@
+
+/* 
+Это кто там такой умный?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Person {
+        void use(Person person);
+
+        void startToWork();
+    }
+
+    interface Workable {
+        boolean wantsToGetExtraWork();
+    }
+
+    interface Secretary extends Person {
+    }
+
+    interface Boss extends Workable, Person {
+    }
+
+    class CleverMan implements Boss   {
+        public void use(Person person) {
+            person.startToWork();
+        }
+
+        public void startToWork() {
+        }
+
+        public boolean wantsToGetExtraWork() {
+            return true;
+        }
+    }
+
+    class SmartGirl implements Secretary {
+        public void use(Person person) {
+        }
+
+        public void startToWork() {
+        }
+    }
+}
